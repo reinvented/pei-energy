4fce4d1d30d9c30e7a3c4d00b5e63691daa16531
Peter Rukavina <peter@rukavina.net>
2011-04-01 17:30:08 -0300

    Added HTML wrapper and explanatory text around audio player.

--- soundcloud/index.php
+++ soundcloud/index.php
@@ -44,10 +44,25 @@ $bpm = (string)$energy->environment->data->current_value;
 $tracks = simplexml_load_file("http://api.soundcloud.com/tracks?consumer_key=" . $souncloud_client_id . "&bpm[from]=$bpm&bpm[to]=$bpm");
 
 /**
+  * Output HTML page.
+  */
+print "<!DOCTYPE html>\n";
+print "<head>\n";
+print "<title>New Brunswick to Prince Edward Island Energy Flow as SoundCloud Tracks</title>\n";
+print "</head>\n";
+print "<body>\n";
+print "<h1>New Brunswick to Prince Edward Island Energy Flow as SoundCloud Tracks</h1>\n";
+print "<p>Takes the current <a href=\"http://www.pachube.com/feeds/21695\">New Brunswick to Prince Edward Island energy interchange in megawatts, and uses this to find <a href=\"soundcloud.com\">SoundCloud.com</a> tracks of that beat-per-minute, allowing you to \"hear\" the flow.</p>\n";
+print "<p>Streams are MP3, so for this to work in a browser, you'll need something that plays MP3 streams in an HTML5 audio element.</p>\n";
+print "<h2>Current Energy Interchange</h2>\n";
+print "<div id=\"energy\">" . $bpm . " MW</div>";
+/**
   * Output the tracks as an HTML5 audio element.
   */
-print "<audio controls=\"controls\">\n";
+print "<audio id=\"audioplayer\" controls=\"controls\">\n";
 foreach($tracks as $key => $track) {
   print "<source src=\"" . $track->{"stream-url"} . "?consumer_key=" . $souncloud_client_id . "\" type=\"audio/mpeg\" />\n";
 }
 print "</audio>\n";
+
+print "</body>";
