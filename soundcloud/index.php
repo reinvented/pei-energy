616e9915d6e5097b955aabe68203c35d918bbf76
Peter Rukavina <peter@rukavina.net>
2011-04-03 12:31:35 -0300

    Added Play and Pause buttons, reworked the UI, converted from HTML audio element to JavaScript-driven audio player, now properly looping through tracks. Limited tracks to 2 minutes or less to add variety.

--- soundcloud/index.php
+++ soundcloud/index.php
@@ -22,7 +22,7 @@
   * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
   * USA
   *
-  * @version 0.1, April 1, 2011
+  * @version 0.2, April 3, 2011
   * @link https://github.com/reinvented/pei-energy
   * @author Peter Rukavina <peter@rukavina.net>
   * @copyright Copyright &copy; 2011, Reinvented Inc.
@@ -37,37 +37,84 @@ require_once("./api-keys-include.php");
   */
 $energy = simplexml_load_file("http://api.pachube.com/v2/feeds/21695/datastreams/PEI.xml?key=" . $pachube_api_key);
 $bpm = (string)$energy->environment->data->current_value;
-
+$bpm = abs($bpm);
 /**
   * Retrieve Soundcloud tracks with the BPM figure we got from Pachube.
   */
-$tracks = simplexml_load_file("http://api.soundcloud.com/tracks?consumer_key=" . $souncloud_client_id . "&bpm[from]=$bpm&bpm[to]=$bpm");
+$tracks = simplexml_load_file("http://api.soundcloud.com/tracks?consumer_key=" . $souncloud_client_id . "&bpm[from]=$bpm&bpm[to]=$bpm&duration[to]=120000");
 
 /**
   * Output HTML page.
   */
-print "<!DOCTYPE html>\n";
-print "<head>\n";
-print "<title>Energy Flow</title>\n";
-print "<link href=\"index.css\" media=\"all\" rel=\"stylesheet\" type=\"text/css\" />\n";
-print "<link rel=\"apple-touch-icon\" href=\"listen-icon.png\" />\n";
-print "<meta name=\"apple-mobile-web-app-capable\" content=\"yes\" />\n";
-print "<meta name=\"viewport\" content=\"width=device-width\">\n";
-print "</head>\n";
-print "<body>\n";
-print "<h1>New Brunswick to Prince Edward Island Energy</h1>\n";
+?>
+<!DOCTYPE html>
+<head>
+<title>Energy Flow</title>
+<link href="index.css" media="all" rel="stylesheet" type="text/css" />
+<link rel="apple-touch-icon" href="listen-icon.png" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="viewport" content="maximum-scale=1.6, width=device-width, user-scalable=no, minimum-scale=1.0">
+<script>
 
-/**
-  * Output the tracks as an HTML5 audio element.
-  */
-print "<audio id=\"audioplayer\" controls=\"controls\" autoplay=\"autoplay\">\n";
-foreach($tracks as $key => $track) {
-  print "<source src=\"" . $track->{"stream-url"} . "?consumer_key=" . $souncloud_client_id . "\" type=\"audio/mpeg\" />\n";
-}
-print "</audio>\n";
+  audio = new Audio();
+  tracks = Array();
+  current = 1;
+  isPlaying = 0;
+  debug = true;
+  
+<?php
+  /**
+    * Loop through the tracks we pulled from SoundCloud and stuff them in an array.
+    * @todo Add the track length and title information so this can be displayed to user.
+    */
+  $tracknumber = 1;
+  foreach($tracks as $key => $track) {
+    if ($track->{"stream-url"}) {
+      print "  tracks[" . $tracknumber . "] = \"" . $track->{"stream-url"} . "?consumer_key=" . $souncloud_client_id . "\";\n";
+      $tracknumber++;
+    }
+  }
+  print "  maxtracks = " . ($tracknumber - 1) . ";\n";
+?>
+  
+  function playTrack() {
+    playpause = document.getElementById('playpause');
+    if (isPlaying) {
+      if (debug) { console.log("isPlaying was true; pausing track " + current) }
+      audio.setAttribute("src", tracks[current]);
+      playpause.setAttribute("src","button-play.png");
+      audio.pause();
+      isPlaying = false;
+    }
+    else {
+      if (debug) { console.log("isPlaying was false; playing track " + current) }
+      audio.setAttribute("src", tracks[current]);
+      playpause.setAttribute("src","button-pause.png");
+      audio.play();
+      isPlaying = true;
+    }
+  }
+  
+  function nextSong() {
+    current++;
+    if (current > maxtracks) {
+      current = 1;
+    }
+    isPlaying = false;
+    if (debug) { console.log("Track ended. Incrementing to track " + current) }
+    playTrack();
+  }
+  
+  audio.addEventListener("ended", nextSong, false);
+  
+</script>
+</head>
+<body>
+<h1>New Brunswick to Prince Edward Island Energy Flow</h1>
+<img onclick="javascript:playTrack()" id="playpause" src="button-play.png" width="128" height="128">
+<?php
 
 print "<h2>Current Energy Interchange</h2>\n";
 print "<div id=\"energy\">" . $bpm . " megawatts</div>";
-print "<p>Takes the current <a href=\"http://www.pachube.com/feeds/21695\">New Brunswick to Prince Edward Island energy interchange</a> in megawatts, and uses this to find <a href=\"soundcloud.com\">SoundCloud.com</a> tracks of that beat-per-minute, allowing you to \"hear\" the flow.</p>\n";
-
+print "<img id=\"pachube\" src=\"http://www.pachube.com/feeds/21695/datastreams/PEI/history.png?w=280&h=120&r=2&s=3&b=true\">";
 print "</body>";
